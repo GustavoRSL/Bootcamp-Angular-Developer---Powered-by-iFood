0615ddadd59764967a592fad8d743ed1b45c8613
GustavoRSL <d7gustavo@yahoo.com.br>
2023-04-23 17:36:54 -0300

    feat: adicionando parte dinâmica dos componentes

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png",
+    "title":"title - 1",
+    "description":"description - 1"
+  },
+  {
+    "id":"2",
+    "photo":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png",
+    "title":"title - 2",
+    "description":"description - 2"
+  },
+  {
+    "id":"3",
+    "photo":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png",
+    "title":"title - 3",
+    "description":"description - 3"
+  },
+  {
+    "id":"4",
+    "photo":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png",
+    "title":"title - 4",
+    "description":"description - 4"
+  }
+]
